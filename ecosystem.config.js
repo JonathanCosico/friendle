3af58a172f0440f1818c17020b3d5d76ff18c619
JonathanCosico <cosicojonathanc@gmail.com>
2026-02-11 16:59:00 -0800

    change from using config to env file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,9 @@ module.exports = {
       name: "friendle-bot",
       script: "index.js",
       env: {
-        NODE_ENV: "prod"
+        NODE_ENV: "prod",
+        DISCORD_TOKEN: process.env.DISCORD_TOKEN,
+        DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
       }
     }
   ]
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits, MessageFlags, Application, ApplicationEmojiManager, ApplicationEmoji, ClientApplication } = require('discord.js');
-const { token } = require('./config.json');
+const token = process.env.DISCORD_TOKEN;
 
 
 // Create a new client instance
